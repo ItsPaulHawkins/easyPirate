23986affb3ac77636ad1191c12aaac4146340610
itspaulhawkins <hawkinsanthonypaul@gmail.com>
2017-12-22 22:32:21 -0500

    Added Maven files.

--- src/main/java/download.java
+++ src/main/java/download.java
@@ -1,5 +1,31 @@
+import java.io.File;
+import java.net.InetAddress;
+import java.nio.file.Path;
+
+import
+import bt.*;
+import bt.dht.DHTConfig;
+import bt.dht.DHTModule;
+import bt.runtime.Config;
+
 public class download {
     public static void main(String args[]){
+        File exportPath = new File("/users/Pablo/Desktop/export");
+        File importPath = new File("magnet.txt");
+        Config config = new Config(){
+            @Override
+            public int getNumOfHashingThreads(){
+                return Runtime.getRuntime().availableProcessors() * 2;
+            }
+
+        };
+        DHTModule dhtModule = new DHTModule(new DHTConfig() {
+            @Override
+            public boolean shouldUseRouterBootstrap() {
+                return true;
+            }
+        });
+        Path targetDirectory = new File("~/Downloads").toPath();
 
     }
 }
